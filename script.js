307f0132fc693daf10c90a755a2db6d3770ce19f
Melvin-KJ <melvinofficial2001@gmail.com>
2024-10-01 08:55:33 +0530

    added products section and parallax effect for moon, text and train elements

---
+++ script.js
@@ -0,0 +1,10 @@
+let moon = document.getElementById("moon");
+let text = document.getElementById("text");
+let train = document.getElementById("train");
+
+window.addEventListener("scroll",()=>{
+    let value = window.scrollY;
+    moon.style.top = value * .9 + "px";
+    text.style.top = 80 + value * -0.2 + '%';
+    train.style.left = value * 1.5 + "px";
+})
